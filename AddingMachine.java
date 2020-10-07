9d24d8d3dd3e3836220503617a136826d80b1be9
MJC04ASU <63020930+MJC04ASU@users.noreply.github.com>
2020-10-07 13:28:01 -0700

    Update AddingMachine.java

--- AddingMachine.java
+++ AddingMachine.java
@@ -2,25 +2,35 @@ package cse360assignment02;
 
 public class AddingMachine {
   private int total;
+  private String totalMachine;
   
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
   }
   
   public int getTotal () {
-    return 0;
+    return total;
   }
   
   public void add (int value) {
+    total += value;
+    totalMachine = totalMaching.concat(" + ");
+    totalMachine = totalMaching.concat(Integer.toString(value));
   }
 
   public void subtract (int value) {
+    total -= value;
+    totalMachine = totalMaching.concat(" - ");
+    totalMachine = totalMaching.concat(Integer.toString(value));
   }
 
   public String toString () {
-    return "";
+    return totalMachine;
   }
 
   public void clear() {
+    total = 0;
+    totalMaching = new String("");
+    
   }
 }
