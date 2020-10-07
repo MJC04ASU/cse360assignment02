e7a5d27e2afe9000a9dfbf959703daf8111ee1ef
MJC04ASU <63020930+MJC04ASU@users.noreply.github.com>
2020-10-07 13:30:48 -0700

    Update AddingMachine.java

--- AddingMachine.java
+++ AddingMachine.java
@@ -2,35 +2,35 @@ package cse360assignment02;
 
 public class AddingMachine {
   private int total;
-  private String totalMachine;
+  private String totalMachine = new String("0");
   
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
   }
   
   public int getTotal () {
-    return total;
+    return total;    //output the current total
   }
   
   public void add (int value) {
-    total += value;
-    totalMachine = totalMaching.concat(" + ");
-    totalMachine = totalMaching.concat(Integer.toString(value));
+    total += value;    //add the new value to the running total
+    totalMachine = totalMaching.concat(" + ");   //add the '+' character to the end of the string.
+    totalMachine = totalMaching.concat(Integer.toString(value));    //add the value to the end of the string.
   }
 
   public void subtract (int value) {
     total -= value;
-    totalMachine = totalMaching.concat(" - ");
-    totalMachine = totalMaching.concat(Integer.toString(value));
+    totalMachine = totalMaching.concat(" - ");    //add the '-' character to the end of the string.
+    totalMachine = totalMaching.concat(Integer.toString(value));    //add the value to the end of the string.
   }
 
   public String toString () {
-    return totalMachine;
+    return totalMachine;    //output the totalMachine string to string.
   }
 
   public void clear() {
-    total = 0;
-    totalMaching = new String("");
+    total = 0;    //reset the addingMaching
+    totalMaching = new String("0"); //reset the totalMaching string
     
   }
 }
